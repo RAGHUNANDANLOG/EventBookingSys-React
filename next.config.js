9df7dee80e6476acab7229dfe2a2786dbdefe920
Raghunandan das <90844992+RAGHUNANDANLOG@users.noreply.github.com>
2025-04-26 21:52:45 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Ensures static export
-  basePath: "/EVENT-BOOKING-SYSTEM", // GitHub repository name
-  assetPrefix: "/EVENT-BOOKING-SYSTEM/", // Ensures assets load correctly
   images: {
-    unoptimized: true, // Fixes image issues in static export
+    unoptimized: true,
   },
   eslint: {
-    ignoreDuringBuilds: true, // Ignores ESLint errors during build
+    ignoreDuringBuilds: true,
   },
 };
 
